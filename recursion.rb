9be99b38d40bc98318dfbc93d85bebf45679ee5f
Dan Holodak <danholodak@gmail.com>
2023-01-25 14:07:59 -0500

    exponent methods

--- recursion.rb
+++ recursion.rb
@@ -18,22 +18,42 @@ def recur_range(min, max)
   #recur_range(1, 1) == []
 end
 
-p range(1, 5) #=> [1,2,3,4]
-p recur_range(1, 2) #=> [1]
-p recur_range(1, 3) #=> [1, 2]
-p recur_range(1, 5) #=> [1,2,3,4]
+# p range(1, 5) #=> [1,2,3,4]
+# p recur_range(1, 2) #=> [1]
+# p recur_range(1, 3) #=> [1, 2]
+# p recur_range(1, 5) #=> [1,2,3,4]
 
 def exp(b, n)
+    return 1 if n == 0
+    b * exp(b, n-1)
 end
 
-# this is math, not Ruby methods.
+# exponent 1
 
-# recursion 1
-p exp(b, 0) #= 1
-p exp(b, n) #= b * exp(b, n - 1)
+# p exp(2, 2) # 4
+# p exp(2, 1) # 2
+# p exp(10, 4)
+# p exp(5, 3)
 
-# recursion 2
-p exp(b, 0) #= 1
-p exp(b, 1) #= b
-p exp(b, n) #= exp(b, n / 2) ** 2             [for even n]
-p exp(b, n) #= b * (exp(b, (n - 1) / 2) ** 2) [for odd n]
+
+# exponent 2
+# p exp(b, 0) #= 1
+# p exp(b, 1) #= b
+# p exp(b, n) #= exp(b, n / 2) ** 2             [for even n]
+# p exp(b, n) #= b * (exp(b, (n - 1) / 2) ** 2) [for odd n]
+
+def exp_2(b, n)
+    return 1 if n == 0
+    if n.even?
+        prev = exp_2(b, n/2)
+        prev * prev
+    else
+        prev = exp_2(b, (n-1)/2)
+        b * prev * prev
+    end
+end
+
+# p exp_2(2, 2) # 4
+# p exp_2(2, 1) # 2
+# p exp_2(10, 4)
+# p exp_2(5, 3)
