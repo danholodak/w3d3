beb428a61119c8dea2b69d08611a319f38c1fd5d
Dan Holodak <danholodak@gmail.com>
2023-01-25 16:17:54 -0500

    fixed bsearch, started merge

--- recursion.rb
+++ recursion.rb
@@ -109,21 +109,39 @@ end
 # p fib(6)
 
 def bsearch(arr, target)
+    return nil if arr.length==0
   mid_idx = arr.length / 2
   mid = arr[mid_idx]
   return mid_idx if mid == target
-  return nil if !arr.include? target
+  #return nil if !arr.include? target
   if mid > target
     bsearch(arr[0...mid_idx], target)
   else
-    bsearch(arr[mid_idx + 1..-1], target) + mid_idx + 1
+    right = bsearch(arr[mid_idx + 1..-1], target)
+    return nil if right == nil
+    right + mid_idx + 1
   end
 end
 
-p bsearch([1, 2, 3], 1) # => 0
-p bsearch([2, 3, 4, 5], 3) # => 1
-p bsearch([2, 4, 6, 8, 10], 6) # => 2
-p bsearch([1, 3, 4, 5, 9], 5) # => 3
-p bsearch([1, 2, 3, 4, 5, 6], 6) # => 5
-p bsearch([1, 2, 3, 4, 5, 6], 0) # => nil
-p bsearch([1, 2, 3, 4, 5, 7], 6) # => nil
+# p bsearch([1, 2, 3], 1) # => 0
+# p bsearch([2, 3, 4, 5], 3) # => 1
+# p bsearch([2, 4, 6, 8, 10], 6) # => 2
+# p bsearch([1, 3, 4, 5, 9], 5) # => 3
+# p bsearch([1, 2, 3, 4, 5, 6], 6) # => 5
+# p bsearch([1, 2, 3, 4, 5, 6], 0) # => nil
+# p bsearch([1, 2, 3, 4, 5, 7], 6) # => nil
+
+def merge_sort(array)
+    return array if array.length <=1
+    mid_idx = array.length/2
+    merge(merge_sort(array[0..mid_idx]),  merge_sort(array[mid_idx+1 ..-1]))
+
+end
+def merge(array_1, array_2)
+    
+    merged = []
+    if array1[0]
+
+end
+
+test_arr = [7,14,3,90]
